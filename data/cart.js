78035a7f274324d525137ed73b4e7c235c5c807a
sagar chetry <chetrysagar177@gmail.com>
2026-09-11 14:33:42 +0530

    Save cart in localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,5 @@
-export let cart =[{
+export let cart = JSON.parse(localStorage.getItem('cart')) ||
+[{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
 },
@@ -6,6 +7,10 @@ export let cart =[{
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
 }];
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 
 export function addToCart(productId) {
@@ -22,6 +27,7 @@ export function addToCart(productId) {
   else {
     cart.push({ productId: productId, quantity: Number(quantityValue.value) });
   }
+  saveToStorage();
 };
 
 
@@ -33,4 +39,5 @@ export function removeFromCart(productId){
     }
   });
   cart = newCart;
+  saveToStorage();
 };
