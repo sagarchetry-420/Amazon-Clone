463af19bd546934c5fbe501d90ba5ee048e7fdcc
sagar chetry <chetrysagar177@gmail.com>
2026-09-20 10:01:46 +0530

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,19 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || [];
+export let cart; //= JSON.parse(localStorage.getItem('cart')) || [];
+
+
+if(!cart){
+  cart = [{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2,
+  deliveryOptionId: '1'
+},
+{
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1,
+  deliveryOptionId: '2'
+}];
+}
+
 
 function saveToStorage(){
   localStorage.setItem('cart',JSON.stringify(cart));
@@ -17,7 +32,7 @@ export function addToCart(productId) {
     matchingItem.quantity += Number(quantityValue.value);
   }
   else {
-    cart.push({ productId: productId, quantity: Number(quantityValue.value) });
+    cart.push({ productId: productId, quantity: Number(quantityValue.value), deliveryOptionId:'1' });
   }
   saveToStorage();
 };
